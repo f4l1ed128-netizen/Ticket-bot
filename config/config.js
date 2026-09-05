9f748f25e4b60ee1d587628e990416f26a1b651e
f4l1ed128-netizen <f4l1ed128@gmail.com>
2026-09-05 06:06:11 +0300

    Fix config formatting and disable claim button

--- config/config.js
+++ config/config.js
@@ -1,31 +1,31 @@
-  module.exports = {
-    clientId: "1545619986901504100", // The id of the discord bot
-    guildId: "1545620208935374931", // The id of the discord server
-    mainColor: "ff0000", // The hex color of the embeds by default
-    lang: "main", // If you want to set english please set "main"
-    openTicketChannelId: "1545620209866637324", // The id of the channel where the message to create a ticket will be sent
-    ticketTypes: [ // You have a limit of 25 types (the limit of Discord)
-      {
-        codeName: "category-one", // The name need to be in lowercase
-        name: "تقديم على الأدارة", // The name that will be displayed in the ticket
-        emoji: "", // The emoji of the type (can be blank)
-        color: "ff0000", // Can be a hex color or blank to use the main color
-        categoryId: "", // The category id where the tickets will be created
-        customDescription: "تقديم على الادارة لسيرفر VIP E-Sports.\n\nReason: REASON", // The custom description of the ticket type (set to blank to use the default description)
-        askReason: false // If the bot should ask the reason of the ticket
-      }
-    ],
-    ticketNameOption: "Ticket-TICKETCOUNT", // Here is all parameter: USERNAME, USERID, TICKETCOUNT
-    rolesWhoHaveAccessToTheTickets: [
-      "1545628470501969990",
-    ], // Roles who can access to the tickets
-    pingRoleWhenOpened: false,
-    roleToPingWhenOpenedId: "", // The role to ping when a ticket is opened
-    logs: true,
-    logsChannelId: "1542605161153957918", // The id of the channel where the logs will be sent
-    claimButton: false,
-    whoCanCloseTicket: "STAFFONLY", // STAFFONLY (roles configured at "rolesWhoHaveAccessToTheTickets") or EVERYONE
-    closeButton: true, // If false the ticket can be closed only by doing /closes
-    askReasonWhenClosing: true, // If false the ticket will be closed without asking the reason
-    maxTicketOpened: 1 // The number of tickets the user can open while another one is already open. Set to 0 to unlimited
-  };
+module.exports = {
+  clientId: "1545619986901504100",
+  guildId: "1545620208935374931",
+  mainColor: "ff0000",
+  lang: "main",
+  openTicketChannelId: "1545620209866637324",
+  ticketTypes: [
+    {
+      codeName: "category-one",
+      name: "تقديم على الأدارة",
+      emoji: "",
+      color: "ff0000",
+      categoryId: "",
+      customDescription: "تقديم على الادارة لسيرفر VIP E-Sports.\n\nReason: REASON",
+      askReason: false
+    }
+  ],
+  ticketNameOption: "Ticket-TICKETCOUNT",
+  rolesWhoHaveAccessToTheTickets: [
+    "1545628470501969990"
+  ],
+  pingRoleWhenOpened: false,
+  roleToPingWhenOpenedId: "",
+  logs: true,
+  logsChannelId: "1542605161153957918",
+  claimButton: false,
+  whoCanCloseTicket: "STAFFONLY",
+  closeButton: true,
+  askReasonWhenClosing: true,
+  maxTicketOpened: 1
+};
